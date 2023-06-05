30d0fb5d8dbc2aaf1578e41a73ea1df7db39303b
Dozixary Lawrence <Dozixary57@hotmail.com>
2023-06-05 20:16:18 +0300

    Добавьте файлы проекта.

---
+++ README.md
@@ -0,0 +1,3 @@
+# NodeJSServerMongoDB
+
+
---
+++ server.js
@@ -0,0 +1,25 @@
+const fastify = require('fastify')({
+    logger: true
+})
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+    reply.send('Hello, world!')
+})
+
+// Run the server!
+fastify.listen({ port: 5000 }, (err, address) => {
+    if (err) throw err
+    console.log(`Server is now listening on ${ address }`)
+})
+
+/*'use strict';
+var http = require('http');
+const { request } = require('https');
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+*/
