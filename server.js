c41a25248a05c09ee1b5121bcad64cbdf6e7dd15
Dozixary Lawrence <Dozixary57@hotmail.com>
2023-06-05 21:27:37 +0300

    Configuring the interaction between the server and the database (output of all elements of the collection and output of the collection element by id).

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
 const fastify = require('fastify')({ logger: true })
 
-// Declare a route
-fastify.get('/', (request, reply) => {
-    reply.send('Hello, world!')
+// Declare a connection to MongoDB
+fastify.register(require('@fastify/mongodb'), {
+    forceClose: true,
+    url: 'mongodb://127.0.0.1:27017',
+    database: 'ReactMongoDB_TEST'
 })
+
+//fastify.get('/Items', async (request, reply) => {
+//    const items = await fastify.mongo.db.collection('items').find().toArray()
+
+//    reply.send(items)
+//})
+
+// Declare a route
+fastify.get('/Item/:id', async function (req, reply) {
+    try {
+        const id = new this.mongo.ObjectId(req.params.id)
+        const item = await fastify.mongo.db.collection('items').findOne({ _id: id })
+        reply.send(item)
+    } catch (err) {
+        reply.send(err)
+    }
+})
+
+// Declare a route
+fastify.get('/Items', async function (req, reply) {
+    try {
+        const items = await fastify.mongo.db.collection('items').find().toArray()
+        reply.send(items)
+    } catch (err) {
+        reply.send(err)
+    }
+})
+
+//// Declare a route
+//fastify.get('/', (request, reply) => {
+//    reply.send('Hello, world!')
+//})
+
+
 
 // Run the server!
 fastify.listen({ port: 5000 }, (err, address) => {
     if (err) throw err
-    console.log(`Server is now listening on ${ address }`)
+    console.log(`Server is now listening on ${address}`)
 })
-
-/*'use strict';
-var http = require('http');
-const { request } = require('https');
-var port = process.env.PORT || 1337;
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
-*/
