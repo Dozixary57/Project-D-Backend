840f589d96c98d2e2961d35a768ff96f5092b0af
Dozixary Lawrence <Dozixary57@hotmail.com>
2023-06-09 16:03:37 +0300

    A working prototype of the server part (checkpoint).

--- routes/route_Items.js
+++ routes/route_Items.js
@@ -3,7 +3,7 @@ module.exports = async function (fastify) {
     fastify.get('/Items', async function (req, reply) {
         try {
             const items = await fastify.mongo.db.collection('items').find().toArray()
-            reply.send(items)
+            reply.status(200).send(items)
         } catch (err) {
             reply.send(err)
         }
@@ -14,9 +14,82 @@ module.exports = async function (fastify) {
         try {
             const id = new this.mongo.ObjectId(req.params.id)
             const item = await fastify.mongo.db.collection('items').findOne({ _id: id })
-            reply.send(item)
+            reply.status(200).send(item)
         } catch (err) {
             reply.send(err)
         }
     })
+
+
+    //// Declare a route
+    //fastify.get('/Item/:id', async function (req, reply) {
+    //    try {
+    //        const id = new this.mongo.ObjectId(req.params.id)
+    //        const item = await fastify.mongo.db.collection('items2').findOne({ _id: id })
+    //        reply.send(item)
+    //    } catch (err) {
+    //        reply.send(err)
+    //    }
+    //})
+
+    //fastify.get('/Items', async function (req, reply) {
+    //    try {
+    //        const items = await fastify.mongo.db.collection('items2').find().toArray()
+    //        if (!items) {
+    //            reply.status(404).send('Items not found');
+    //            return
+    //        }
+    //        reply.status(200).send(items)
+    //    } catch (err) {
+    //        reply.send(err)
+    //    }
+    //});
+
+    //fastify.get('/Item/:id', async function (req, reply) {
+    //    const id = new this.mongo.ObjectId(req.params.id);
+    //    try {
+    //        const item = await fastify.mongo.db.collection('items2').findOne({ _id: id });
+    //        if (!item) {
+    //            const item = await fastify.mongo.db.collection('items2').findOne({ itemTitle: 'Stick' });
+    //            if (!Title) {
+    //                reply.status(404).send('Item not found');
+    //                return
+    //            }
+    //            reply.redirect(301, `/Item/${Title.itemTitle}`).send(Title)
+    //            return
+    //        }
+    //        reply.status(200).send(item);
+    //    } catch (err) {
+    //        reply.send(err);
+    //    }
+    //});
+
+    //fastify.get('/Item/:id', async function (req, reply) {
+    //    try {
+    //        const idOrTitle = new this.mongo.ObjectId(req.params.id);
+    //        const item = await fastify.mongo.db.collection('items2').findOne({ _id: idOrTitle })
+    //        if (item) {
+    //            reply.redirect(301, `/Item/${item.itemTitle}`).send(item)
+    //            return
+    //        } else {
+    //            const item = await fastify.mongo.db.collection('items2').findOne({ itemTitle: idOrTitle })
+    //            if (item) {
+    //                reply.redirect(301, `/Item/${item.itemTitle}`).send(item)
+    //                return
+    //            }
+    //        }
+    //        //if (!item) {
+    //        //    const item = await fastify.mongo.db.collection('items2').findOne({ itemTitle: 'Stick' });
+    //        //    if (!Title) {
+    //        //        reply.status(404).send('Item not found');
+    //        //        return
+    //        //    }
+    //        //    reply.redirect(301, `/Item/${Title.itemTitle}`).send(Title)
+    //        //    return
+    //        //}
+    //        /*reply.status(200).send(item);*/
+    //    } catch (err) {
+    //        reply.send(err);
+    //    }
+    //});
 }
