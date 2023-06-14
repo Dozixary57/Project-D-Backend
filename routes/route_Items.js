356a5fca79a48037ccc4a97d03af29dde03f28e3
Dozixary Lawrence <Dozixary57@hotmail.com>
2023-06-14 15:13:31 +0300

    Configured route forwarding ("ObjectId" => "Title").

--- routes/route_Items.js
+++ routes/route_Items.js
@@ -16,25 +16,23 @@ module.exports = async function (fastify) {
     fastify.get('/Item/:id', async function (req, reply) {
         // http://localhost:5000/Item/64825519e97f28274a958dca
         try {
-            let item = await fastify.mongo.db.collection('items2').findOne({
-                itemTitle: req.params.id.replace(/_/g, ' ')
-            });
+            let item = await fastify.mongo.db.collection('items2').findOne({ itemTitle: req.params.id.replace(/_/g, ' ') })
             if (item) {
-                reply.status(200).send(item);
-                return;
+                reply.status(200).send(item)
+                return
             } else {
                 const id = new this.mongo.ObjectId(req.params.id)
-                item = await fastify.mongo.db.collection('items2').findOne({ _id: id });
+                item = await fastify.mongo.db.collection('items2').findOne({ _id: id })
                 if (item) {
-                    reply.status(200).send(item);
-                    return;
+                    reply.redirect(301, `${item.itemTitle.replace(/ /g, '_')}`).send(item)
+                    return
                 } else {
-                    reply.status(404).send('Item not found.');
-                    return;
+                    reply.status(404).send('Item not found.')
+                    return
                 }
             }
         } catch {
-            reply.status(404).send('Item not found.');
+            reply.status(404).send('Item not found.')
         }
     })
 }
